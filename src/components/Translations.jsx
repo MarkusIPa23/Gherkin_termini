8fcf1c18bc521b28448637eaa8bd346280c4772e
Markus <ipa23.m.spulis@vtdt.edu.lv>
2026-01-22 15:04:03 +0200

    Jaunākās izmaiņas

--- src/components/Translations.jsx
+++ src/components/Translations.jsx
@@ -3,11 +3,18 @@ import TranslationsTable from "./TranslationsTable";
 const gherkinTerms = [
   { en: "Feature", lv: "Funkcionālā iezīme" },
   { en: "Scenario", lv: "Scenārijs" },
-  { en: "Given", lv: "Ņemot vērā" },
-  { en: "When", lv: "Kad" },
+  { en: "Given", lv: "Kad" },
+  { en: "When", lv: "Ja" },
   { en: "Then", lv: "Tad" },
   { en: "And", lv: "Un" },
+  { en: "Or", lv: "Vai" },
   { en: "But", lv: "Bet" },
+  { en: "Background", lv: "Konteksts" },
+  { en: "Rule", lv: "Noteikums" },
+  { en: "Scenario Outline", lv: "Scenārijs pec parauga" },
+  { en: "Examples", lv: "Piemeri" },
+  { en: "User Story", lv: "Lietotājastāsts" },
+  { en: "#", lv: "Komentārs" },
 ];
 
 function Translations() {
